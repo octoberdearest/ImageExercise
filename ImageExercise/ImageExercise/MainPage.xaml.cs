8687d83efb37e43cf71104908f2deef5800b9d25
maerose.bibit <maerose.bibit@service101plus.com>
2017-10-05 16:23:30 +0800

    Add project files.

---
+++ ImageExercise/ImageExercise/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageExercise
+{
+    public partial class MainPage : ContentPage
+    {
+        private int counter = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+            counter = 1;
+            LoadImage();
+        }
+
+        public void LoadImage()
+        {
+            myImage.Source = new UriImageSource()
+            {
+                Uri = new Uri($"http://lorempixel.com/1920/1080/city/{counter}"),
+                CachingEnabled = false
+            };
+        }
+
+        private void Button_OnClickedPrevious(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == 0)
+                counter = 10;
+
+            LoadImage();
+        }
+
+        private void Button_OnClickedNext(object sender, EventArgs e)
+        {
+            counter++;
+            if (counter > 10)
+                counter = 1;
+
+            LoadImage();
+        }
+    }
+}
